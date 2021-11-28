e417f2e8d3f0359e031d323a08159e1eefed8578
Allan Legemaate <alegemaate@gmail.com>
2021-11-28 16:30:40 -0500

    Add texture atlas to config

--- src/game/Tile.cpp
+++ src/game/Tile.cpp
@@ -17,11 +17,3 @@ void Tile::jiggle(char newXJiggle, char newYJiggle, char newZJiggle) {
 void Tile::setType(TileType* val) {
   tileImpl = val;
 }
-
-// Draw tile
-void Tile::draw(int tick) {
-  tileImpl->draw(static_cast<float>(x) + static_cast<float>(xJiggle) / 100.0f,
-                 static_cast<float>(y) + static_cast<float>(yJiggle) / 100.0f,
-                 static_cast<float>(z) + static_cast<float>(zJiggle) / 100.0f,
-                 tick);
-}
--- src/game/TileTypeManager.cpp
+++ src/game/TileTypeManager.cpp
@@ -35,61 +35,31 @@ void TileTypeManager::load(const std::string& newFile) {
   std::cout << "   TILES\n-------------\n";
 
   // Load tiles
-  for (rapidxml::xml_node<>* cTile = allTiles->first_node("tile"); cTile;
-       cTile = cTile->next_sibling()) {
-    // Read xml variables
+  for (rapidxml::xml_node<>* xmlTile = allTiles->first_node("tile"); xmlTile;
+       xmlTile = xmlTile->next_sibling()) {
     // General
-    int tileID = atoi(cTile->first_attribute("id")->value());
-    std::string name = cTile->first_node("name")->value();
-    std::string image1 =
-        cTile->first_node("images")->first_node("image")->value();
-    std::string image2 =
-        cTile->first_node("images")->first_node("image2")->value();
-    std::string model = cTile->first_node("model")->value();
-    int randomness = atoi(cTile->first_node("random")->value());
+    int id = atoi(xmlTile->first_attribute("id")->value());
+    std::string name = xmlTile->first_node("name")->value();
 
-    std::string attribute_string = cTile->first_node("attribute")->value();
-    int attribute = ATTRIBUTE_GAS;
+    // Atlas ids
+    rapidxml::xml_node<>* xmlAtlas = xmlTile->first_node("atlas");
 
-    std::cout << attribute_string;
+    AtlasLookup atlasIds;
+    atlasIds[0] = atoi(xmlAtlas->first_attribute("top")->value());
+    atlasIds[1] = atoi(xmlAtlas->first_attribute("bottom")->value());
+    atlasIds[2] = atoi(xmlAtlas->first_attribute("left")->value());
+    atlasIds[3] = atoi(xmlAtlas->first_attribute("right")->value());
+    atlasIds[4] = atoi(xmlAtlas->first_attribute("front")->value());
+    atlasIds[5] = atoi(xmlAtlas->first_attribute("back")->value());
 
-    // Get attribute
-    if (attribute_string == "ATTRIBUTE_GAS") {
-      attribute = ATTRIBUTE_GAS;
-    } else if (attribute_string == "ATTRIBUTE_SOLID") {
-      attribute = ATTRIBUTE_SOLID;
-    } else if (attribute_string == "ATTRIBUTE_LIQUID") {
-      attribute = ATTRIBUTE_LIQUID;
-    }
-
-    // Draw to screen (debug)
-    std::cout << "-> Loading Tile:" << name << "  ID:" << tileID
-              << "  MODEL:" << model << "  ATTRIBUTE:" << attribute_string
-              << "  RANDOMNESS:" << randomness << "\n";
-
-    ModelType modelType;
-    if (model == "MODEL_CUBE_SMALL") {
-      modelType = ModelType::MODEL_CUBE_SMALL;
-    } else if (model == "MODEL_FLAT") {
-      modelType = ModelType::MODEL_FLAT;
-    } else if (model == "MODEL_DOUBLE_PLANE") {
-      modelType = ModelType::MODEL_DOUBLE_PLANE;
-    } else if (model == "MODEL_DOUBLE_PLANE_TALL") {
-      modelType = ModelType::MODEL_DOUBLE_PLANE_TALL;
-    } else if (model == "MODEL_CUBE") {
-      modelType = ModelType::MODEL_CUBE;
-    } else {
-      modelType = ModelType::MODEL_NONE;
-    }
+    // Log
+    std::cout << "-> Loading Tile:" << name << "  ID:" << id << std::endl;
 
     // Create tile, set variables and add it to the tile list
-    TileType newTileType(name, tileID, modelType, attribute, randomness);
-
-    // Set images
-    newTileType.setImages(image1, image2);
+    TileType tile(id, atlasIds);
 
     // Add the tile
-    tile_defs.push_back(newTileType);
+    tile_defs.push_back(tile);
   }
 
   std::cout << "\n\n";
